4b253ca39621f7ebe17d8bebdcc76a87f4a52194
nicola <nicolamorelli30008@gmail.com>
2025-01-30 14:44:01 +0100

    fixes

--- source/utils.py
+++ source/utils.py
@@ -1,3 +1,6 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from prettytable import PrettyTable
 from wilds import get_dataset
 import torch
@@ -8,6 +11,7 @@ from sklearn.cluster import KMeans
 from tqdm import tqdm
 import numpy as np
 from scipy.stats import mode
+
 
 
 def display_configs(configs):
