5627f886389905b4ada8ded9aa99cbdcd82f9fbf
Matteo <matteolugli18@gmail.com>
2025-01-31 16:21:26 +0100

    added nce loss. Debugged locally (should work fine on server)

--- source/utils.py
+++ source/utils.py
@@ -49,6 +49,50 @@ def contrastive_loss(features, device, temperature=0.5):
     loss = F.cross_entropy(similarity_matrix, labels)
     return loss
 
+def info_nce_loss(features, device, temperature=0.5):
+    """
+    Implements Noise Contrastive Estimation loss as explained in the simCLR paper.
+    Actual code is taken from here https://github.com/sthalles/SimCLR/blob/master/simclr.py
+    Args:
+        - features: torch tensor of shape (2*N, D) where N is the batch size.
+            The first N samples are the original views, while the last
+            N are the modified views.
+        - device: torch device
+        - temperature: float
+    """
+    n_views = 2
+    assert features.shape[0] % n_views == 0 # make sure shapes are correct
+    batch_size = features.shape[0] // n_views
+
+    labels = torch.cat([torch.arange(batch_size) for i in range(n_views)], dim=0)
+    labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
+    labels = labels.to(device)
+
+    features = F.normalize(features, dim=1)
+
+    similarity_matrix = torch.matmul(features, features.T)
+    assert similarity_matrix.shape == (
+        n_views * batch_size, n_views * batch_size)
+    assert similarity_matrix.shape == labels.shape
+
+    # discard the main diagonal from both: labels and similarities matrix
+    mask = torch.eye(labels.shape[0], dtype=torch.bool).to(device)
+    labels = labels[~mask].view(labels.shape[0], -1)
+    similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+    # assert similarity_matrix.shape == labels.shape
+
+    # select and combine multiple positives
+    positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
+
+    # select only the negatives the negatives
+    negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+
+    logits = torch.cat([positives, negatives], dim=1)
+    labels = torch.zeros(logits.shape[0], dtype=torch.long).to(device)
+
+    logits = logits / temperature
+    return F.cross_entropy(logits, labels)
+
 
 def config_loader(config):
     net, loss, opt = ..., ..., ...
@@ -56,7 +100,9 @@ def config_loader(config):
         net = SimCLR()
 
     if str(config["loss"]).__contains__("contrastive"):
-        loss = contrastive_loss
+        loss = contrastive_loss    
+    if str(config["loss"]).__contains__("NCE"):
+        loss = info_nce_loss
 
     if str(config["opt"]).__contains__("adam"):
         opt = torch.optim.Adam(net.parameters(), lr=0.005)
