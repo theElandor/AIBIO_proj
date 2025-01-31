034c1139b9aeb11a13ebb3e52d65aeb9ee36bece
Nicola Morelli <nicolamorelli30008@gmail.com>
2025-01-31 18:25:16 +0100

    added checkpoint loading, aand modularized training

--- source/train.py
+++ source/train.py
@@ -15,69 +15,126 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 torch.manual_seed(42)
-inFile = sys.argv[1]
-with open(inFile, "r") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
-display_configs(config)
-device = load_device(config)
-dataset = Rxrx1(config['dataset_dir'])
 
-train_size = int(0.7 * len(dataset))
-val_size = int(0.15 * len(dataset))
-test_size = len(dataset) - train_size - val_size
-train_dataset, val_dataset, test_dataset = random_split(
-    dataset, [train_size, val_size, test_size])
 
-train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'])
+def load_yaml():
+    inFile = sys.argv[1]
+    with open(inFile, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    display_configs(config)
+    return config
 
-net, loss_func, opt = config_loader(config)
-# no transformations
-standard = transforms.Compose(
-    [transforms.ToImage(), transforms.ToDtype(torch.float32, scale=True)])
-# view for self supervised learning
-transform = transforms.Compose([
-    transforms.RandomResizedCrop(256),
-    transforms.ColorJitter(brightness=0.5, contrast=0.5),
-    transforms.GaussianBlur(3, sigma=(0.1, 2.0)),
-    transforms.Compose(
+
+class Trainer():
+    def __init__(self, net, device, config, opt, loss_func, scheduler=None):
+        self.net = net
+        self.device = device
+        self.config = config
+        self.opt = opt
+        self.loss_func = loss_func
+        self.scheduler = scheduler
+
+    def load_checkpoint(self):
+        checkpoint = torch.load(self.config['load_checkpoint'])
+        if self.config['multiple_gpus']:
+            model_dict = {key.replace(
+                "module.", ""): value for key, value in checkpoint['model_state_dict'].items()}
+            self.net.load_state_dict(model_dict)
+        else:
+            self.net.load_state_dict(checkpoint['model_state_dict'])
+
+        self.opt.load_state_dict(checkpoint['optimizer_state_dict'])
+        try:
+            self.scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        except:
+            print("Scheduler not found in the checkpoint.")
+
+        last_epoch = checkpoint['epoch']
+        training_loss_values = checkpoint['training_loss_values']
+        validation_loss_values = checkpoint['validation_loss_values']
+        config['batch_size'] = checkpoint['batch_size']
+        return (last_epoch, training_loss_values, validation_loss_values)
+
+    def train(self, split_sizes, dataset, transform):
+
+        train_size = int(split_sizes[0] * len(dataset))
+        val_size = int(split_sizes[1] * len(dataset))
+        test_size = len(dataset) - train_size - val_size
+        train_dataset, val_dataset, test_dataset = random_split(
+            dataset, [train_size, val_size, test_size])
+
+        train_dataloader = DataLoader(
+            train_dataset, batch_size=config['batch_size'])
+
+
+
+        if 'load_checkpoint' in self.config.keys():
+            print('Loading latest checkpoint... ')
+            last_epoch, training_loss_values, validation_loss_values = self.load_checkpoint()
+            print(f"Checkpoint {config['load_checkpoint']} Loaded")
+        else:
+            last_epoch = 0
+            training_loss_values = []  # store every training loss value
+            validation_loss_values = []  # store every validation loss value
+
+        self.net = self.net.to(device)
+        self.net.train()
+        if self.config['multiple_gpus']:
+            self.net = nn.DataParallel(self.net)
+
+        for epoch in range(last_epoch, int(self.config['epochs'])):
+            pbar = tqdm(total=len(train_dataloader), desc=f"Epoch-{epoch}")
+            for x_batch, _, _ in train_dataloader:
+                standard_views = torch.cat(
+                    [standard(img.unsqueeze(0)) for img in x_batch], dim=0).to(device)
+                augmented_views = torch.cat(
+                    [transform(img.unsqueeze(0)) for img in x_batch], dim=0).to(device)
+                block = torch.cat([standard_views, augmented_views], dim=0)
+                out_feat = self.net.forward(block.to(torch.float))
+                loss = self.loss_func(out_feat, device)
+
+                self.opt.zero_grad()
+                loss.backward()
+                self.opt.step()
+                training_loss_values.append(loss.item())
+
+                pbar.update(1)
+                pbar.set_postfix({'Loss': loss.item()})
+
+            if (epoch + 1) % int(config['evaluation_freq']) == 0:
+                print(f"Running Validation...")
+                validation_loss_values += validation_loss(self.net, DataLoader(val_dataset, batch_size=self.config['batch_size']),
+                                                          self.device, transform, self.loss_func)
+
+            if (epoch + 1) % int(self.config['model_save_freq']) == 0:
+                save_model(epoch, self.net, self.opt, training_loss_values,
+                           validation_loss_values, self.config['batch_size'], self.config['checkpoint_dir'], self.config['opt'])
+                test_kmean_accuracy(self.net.backbone, DataLoader(
+                    test_dataset, batch_size=self.config['batch_size']), self.device)
+
+        return training_loss_values, validation_loss_values
+
+
+if __name__ == "__main__":
+
+    config = load_yaml()
+    device = load_device(config)
+    dataset = Rxrx1(config['dataset_dir'])
+    net, loss_func, opt = config_loader(config)
+
+    # no transformations
+    standard = transforms.Compose(
         [transforms.ToImage(), transforms.ToDtype(torch.float32, scale=True)])
-])
-net.to(device)
-# test_kmean_accuracy(net.backbone, DataLoader(
-    # test_dataset, batch_size=config['batch_size']), device)
-net.train()
-if config['multiple_gpus']:
-    net = nn.DataParallel(net)
+    # view for self supervised learning
+    transform = transforms.Compose([
+        transforms.RandomResizedCrop(256),
+        transforms.ColorJitter(brightness=0.5, contrast=0.5),
+        transforms.GaussianBlur(3, sigma=(0.1, 2.0)),
+        transforms.Compose(
+            [transforms.ToImage(), transforms.ToDtype(torch.float32, scale=True)])
+    ])
 
-training_loss_values = []  # store every training loss value
-validation_loss_values = []  # store every validation loss value
+    tr_ = Trainer(net, device, config, opt, loss_func)
 
-for epoch in range(int(config['epochs'])):
-    pbar = tqdm(total=len(train_dataloader), desc=f"Epoch-{epoch}")
-    for x_batch, _, _ in train_dataloader:
-        standard_views = torch.cat(
-            [standard(img.unsqueeze(0)) for img in x_batch], dim=0).to(device)
-        augmented_views = torch.cat(
-            [transform(img.unsqueeze(0)) for img in x_batch], dim=0).to(device)
-        block = torch.cat([standard_views, augmented_views], dim=0)
-        out_feat = net.forward(block.to(torch.float))
-        loss = loss_func(out_feat, device)
-
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-        training_loss_values.append(loss.item())
-
-        pbar.update(1)
-        pbar.set_postfix({'Loss': loss.item()})
-
-    if (epoch + 1) % int(config['evaluation_freq']) == 0:
-        print(f"Running Validation...")
-        validation_loss_values += validation_loss(net, DataLoader(test_dataset, batch_size=config['batch_size']),
-                                                  device, transform, loss_func)
-
-    if (epoch + 1) % int(config['model_save_freq']) == 0:
-        save_model(epoch, net, opt, training_loss_values,
-                   validation_loss_values, config['batch_size'], config['checkpoint_dir'], config['opt'])
-        test_kmean_accuracy(net.backbone, DataLoader(
-            test_dataset, batch_size=config['batch_size']), device)
+    training_loss_values, validation_loss_values = tr_.train(
+        [0.7, 0.15, 0.15], dataset, transform)
