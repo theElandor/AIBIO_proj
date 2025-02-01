c2f85c3248a03dfab6f0a1b2111de7b41602eb19
Nicola Morelli <nicolamorelli30008@gmail.com>
2025-02-01 11:12:29 +0100

    fixes

--- source/train.py
+++ source/train.py
@@ -23,7 +23,7 @@ def load_yaml():
     assert Path(config['checkpoint_dir']).is_dir(), "Please provide a valid directory to save checkpoints in."
     assert Path(config['dataset_dir']).is_dir(), "Please provide a valid directory to load dataset."
     if 'load_checkpoint' in config.keys():
-        assert Path(config['load_checkpoint']).is_dir(), "Please provide a valid directory to load dataset."
+        assert Path(config['load_checkpoint']).is_file(), "Please provide a valid file to load checkpoint."
 
     return config
 
@@ -66,13 +66,13 @@ class Trainer():
         train_dataset, val_dataset, test_dataset = random_split(
             dataset, [train_size, val_size, test_size])
 
-        train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], pin_memory_device=self.device, pin_memory=True,
-                                      shuffle=True, num_workers=4, drop_last=True, prefetch_factor=1)
+        train_dataloader = DataLoader(train_dataset, batch_size=self.config['batch_size'], pin_memory_device=self.device, pin_memory=True,
+                                      shuffle=True, num_workers=8, drop_last=True, prefetch_factor=1)
 
         if 'load_checkpoint' in self.config.keys():
             print('Loading latest checkpoint... ')
             last_epoch, training_loss_values, validation_loss_values = self.load_checkpoint()
-            print(f"Checkpoint {config['load_checkpoint']} Loaded")
+            print(f"Checkpoint {self.config['load_checkpoint']} Loaded")
         else:
             last_epoch = 0
             training_loss_values = []  # store every training loss value
@@ -105,10 +105,14 @@ class Trainer():
             if (epoch + 1) % int(self.config['model_save_freq']) == 0:
                 save_model(epoch, self.net, self.opt, training_loss_values, validation_loss_values,
                            self.config['batch_size'], self.config['checkpoint_dir'], self.config['opt'])
-                test_kmean_accuracy(self.net.backbone, DataLoader(test_dataset, batch_size=self.config['batch_size'], pin_memory_device=self.device, pin_memory=True,
-                                                                  shuffle=True, num_workers=4, drop_last=True, prefetch_factor=1), self.device)
+                if self.config['multiple_gpus']:
+                    backbone = self.net.module.backbone
+                else:
+                    backbone = self.net.backbone
+                test_kmean_accuracy(backbone, DataLoader(test_dataset, batch_size=self.config['batch_size'], pin_memory_device=self.device, pin_memory=True,
+                                                         shuffle=True, num_workers=4, drop_last=True, prefetch_factor=1), self.device)
 
-            if (epoch + 1) % int(config['evaluation_freq']) == 0:
+            if (epoch + 1) % int(self.config['evaluation_freq']) == 0:
                 print(f"Running Validation...")
                 validation_loss_values += validation_loss(self.net, DataLoader(val_dataset, batch_size=self.config['batch_size'], pin_memory_device=self.device, pin_memory=True,
                                                                                shuffle=True, num_workers=4, drop_last=True, prefetch_factor=1), self.device, transform, std_transform, self.loss_func)
