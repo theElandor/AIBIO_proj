06194fa961b418880f7d2dff71a5774015751dd9
Matteo Lugli <matteolugli18@gmail.com>
2025-02-11 17:30:53 +0100

    added collate function to config file

--- config/slurm/head_train.sh
+++ config/slurm/head_train.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#SBATCH -e /homes/nmorelli/output/err.txt
-#SBATCH -o /homes/nmorelli/output/out.txt
+#SBATCH -e /homes/mlugli/output/err.txt
+#SBATCH -o /homes/mlugli/output/out.txt
 #SBATCH --job-name=simclr_head_train
 #SBATCH --account=ai4bio2024
 #SBATCH --partition=all_usr_prod
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:2
-#SBATCH --mem=20G
+#SBATCH --gres=gpu:1
+#SBATCH --mem=200G
 #SBATCH --time=24:00:00
 
-python3 /homes/nmorelli/AIBIO_proj/source/train_classifier.py /homes/nmorelli/AIBIO_proj/config/train/head_conf.yaml
+python3 /homes/mlugli/AIBIO_proj/source/train_classifier_norm.py /homes/mlugli/AIBIO_proj/config/train/head_train_norm.yaml
