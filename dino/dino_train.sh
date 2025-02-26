435f95ee36086b2388619f4277d9653ed092fc7c
Matteo Lugli <matteolugli18@gmail.com>
2025-02-26 10:06:37 +0100

    fixed sh script

--- dino/dino_train.sh
+++ dino/dino_train.sh
@@ -9,4 +9,4 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=200G
 #SBATCH --time=24:00:00
-python3 /homes/mlugli/dino/main_dino.py --arch vit_small --saveckp_freq 2 --data_path /work/ai4bio2024/rxrx1 --output_dir /work/h2020deciderficarra_shared/rxrx1/checkpoints/dino
+python3 /homes/mlugli/AIBIO_proj/dino/main_dino.py --arch vit_small --saveckp_freq 2 --data_path /work/ai4bio2024/rxrx1 --output_dir /work/h2020deciderficarra_shared/rxrx1/checkpoints/dino/cross_batch_1
